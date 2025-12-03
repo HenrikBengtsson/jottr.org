aed7a2e68d966d8a745edaf0d5fa2aef4b7caa66
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2025-12-03 14:55:39 -0800

    Tweak styling of R script

--- static/post/2021-06-10-parallelly_1.26.0.R
+++ static/post/2021-06-10-parallelly_1.26.0.R
@@ -18,14 +18,14 @@ ncores <- unique(sort(c(ncores, ncores - c(0, 0, diff(diff(ncores))))))
 
 ## The maximum number of workers we can use is limited by the number
 ## of connections can open.
-reserveConnections <- 4L  ## AD HOC: Need to reserve a few more
+reserveConnections <- 4L ## AD HOC: Need to reserve a few more
 maxConnections <- freeConnections() - reserveConnections
 ncores[ncores >= maxConnections] <- maxConnections
 ncores <- unique(ncores)
 max_ncores <- max(ncores)
 message("Number of cores benchmarked: ", paste(ncores, collapse = ", "))
 
-tags <- c(ncores=max_ncores, B=B, hostname=Sys.info()[["nodename"]])
+tags <- c(ncores = max_ncores, B = B, hostname = Sys.info()[["nodename"]])
 tags <- sprintf("%s=%s", names(tags), tags)
 tags <- paste(tags, collapse = "_")
 name <- sprintf("stats_%s", tags)
@@ -37,41 +37,59 @@ fcns <- list(
 fcns <- fcns[c("makeClusterPSOCK")]
 
 with_progress({
-p <- progressor(B * length(fcns) * length(ncores) * 2L)
-stats <- list()
-for (bb in 1:B) {
-  for (ii in seq_along(ncores)) {
-    n <- ncores[ii]
-    for (setup_strategy in c("sequential", "parallel")) {
-      for (method in names(fcns)) {
-        fcn <- fcns[[method]]
-        dt <- system.time({
-          cl <- fcn(n, rscript_args = "--vanilla", setup_strategy = setup_strategy)
-        })[["elapsed"]]
-        parallel::stopCluster(cl)
-        stats_t <- data.frame(cores = n, method = method, setup_strategy = setup_strategy, time = dt)
-        stats <- c(stats, list(stats_t))
-        p(sprintf("n=%d, method = %s, %s, iteration=%d", n, method, setup_strategy, bb))
-        gc()
-      } ## for (method ...)
+  p <- progressor(B * length(fcns) * length(ncores) * 2L)
+  stats <- list()
+  for (bb in 1:B) {
+    for (ii in seq_along(ncores)) {
+      n <- ncores[ii]
+      for (setup_strategy in c("sequential", "parallel")) {
+        for (method in names(fcns)) {
+          fcn <- fcns[[method]]
+          dt <- system.time({
+            cl <- fcn(
+              n,
+              rscript_args = "--vanilla",
+              setup_strategy = setup_strategy
+            )
+          })[["elapsed"]]
+          parallel::stopCluster(cl)
+          stats_t <- data.frame(
+            cores = n,
+            method = method,
+            setup_strategy = setup_strategy,
+            time = dt
+          )
+          stats <- c(stats, list(stats_t))
+          p(sprintf(
+            "n=%d, method = %s, %s, iteration=%d",
+            n,
+            method,
+            setup_strategy,
+            bb
+          ))
+          gc()
+        } ## for (method ...)
+      }
     }
   }
-}
 })
 
 stats <- do.call(rbind, stats)
-stats$setup_strategy <- factor(stats$setup_strategy, levels = c("sequential", "parallel"))
+stats$setup_strategy <- factor(
+  stats$setup_strategy,
+  levels = c("sequential", "parallel")
+)
 stats <- tibble::as.tibble(stats)
 saveRDS(stats, file = sprintf("%s.rds", name))
 print(stats, n = 10e3)
 
 library(ggplot2)
-gg <- ggplot(stats, aes(x=cores, y=time, color=setup_strategy))
+gg <- ggplot(stats, aes(x = cores, y = time, color = setup_strategy))
 gg <- gg + geom_smooth(size = 2, method = "loess", formula = y ~ x)
 gg <- gg + xlab("Number of parallel workers")
 gg <- gg + ylab("Total setup time (s)")
 gg <- gg + labs(color = "Setup strategy")
-ggsave(gg, filename = sprintf("%s.png", name), width = 7, height = 7/1.3)
+ggsave(gg, filename = sprintf("%s.png", name), width = 7, height = 7 / 1.3)
 print(gg)
 
 
@@ -84,16 +102,16 @@ coeffs <- lapply(strategies, FUN = function(strategy) {
 names(coeffs) <- strategies
 print(coeffs)
 ## $sequential
-## (Intercept)       cores 
+## (Intercept)       cores
 ## -0.07208812  0.42562891
-## 
+##
 ## $parallel
-## (Intercept)       cores 
+## (Intercept)       cores
 ##  0.39637113  0.00594509
 
 print(by(stats$time, INDICES = stats$setup_strategy, FUN = max))
 ## stats$setup_strategy: sequential
 ## [1] 51.971
-## ------------------------------------------------------------ 
+## ------------------------------------------------------------
 ## stats$setup_strategy: parallel
 ## [1] 1.157
